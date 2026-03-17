195b7ffa7ed39bc7b937585fdacb5b47b435f932
jorge <toxicpotato1223@gmail.com>
2026-03-17 14:06:20 +0800

    fix: lowercase header and footer imports for Linux

--- resources/js/Pages/LandingPage.jsx
+++ resources/js/Pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
-import Header from "../components/layouts/Header";
-import Footer from "../components/layouts/Footer";
+import Header from "../components/layouts/header";
+import Footer from "../components/layouts/footer";
 
 import HeroSection from "./LandingPage/HeroSection";
 import CategoriesSection from "./LandingPage/CategoriesSection";
